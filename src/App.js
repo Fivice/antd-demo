e3e072c5d53d7f8f1733e6a14cd7c552e5a7a8d8
Snailoct <wubin_oct@163.com>
2019-07-09 16:00:12 +0800

    引入less加载插件，以及按需加载配置

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './App.less';
 import { Button } from 'antd';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Button type="primary">按钮</Button>
-    </div>
-  );
-}
 
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Button type="primary">按钮</Button>
+      </div>
+    )
+  }
+}
 export default App;
